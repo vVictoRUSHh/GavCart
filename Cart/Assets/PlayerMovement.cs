6728dc50d403b359bca3e3cdaa2ebc708914bd1a
vVictoRUSHh <stoian_vitea2003@mail.ru>
2023-07-11 01:19:42 +0300

    fdf

--- Cart/Assets/PlayerMovement.cs
+++ Cart/Assets/PlayerMovement.cs
@@ -23,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 8000f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
