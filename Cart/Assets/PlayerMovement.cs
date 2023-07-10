0f58a8bed597995e4a1b178b38c5fdedda611c3a
vVictoRUSHh <stoian_vitea2003@mail.ru>
2023-07-11 00:28:36 +0300

    Zalupa Comit

--- Cart/Assets/PlayerMovement.cs
+++ Cart/Assets/PlayerMovement.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private float turnSmoothVelocity;
+    private Vector3 moveDirection;
+    public Animator animator;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
     }
+
 }
